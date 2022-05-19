b964c120e5a3189c855295bc0a096123f8a0d4e2
PrajwalMahajan477 <105885489+PrajwalMahajan477@users.noreply.github.com>
2022-05-20 01:14:54 +0530

    new

---
+++ 001.php
@@ -0,0 +1,15 @@
+<?php
+  $x=7;
+  $y=7;
+ echo $x+$y;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>wfrgreger</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ index1.php
@@ -0,0 +1,19 @@
+<?php
+header('Location: new1.html');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Animated Login Form</title>
+	<link rel="stylesheet" type="text/css" href="Login.css">
+</head>
+<body>
+	<form class="box" action="index1.php" method="POST">
+	<h1>Login</h1>
+	   <input type="text" name="" placeholder="username">
+	   <input type="password" name="" placeholder="password">
+	   <button type="submit">Login</button>
+	</form>
+</body>
+</html>
